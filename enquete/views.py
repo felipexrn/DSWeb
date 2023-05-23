a7a6921d5fda5d5dfa556f91b271722ddb717444
felipexrn <felipe.x@escolar.ifrn.edu.br>
2023-05-23 15:59:53 +0000

    preguiçaaaaaaaaaaaaaaaaaaaaaaa

--- enquete/views.py
+++ enquete/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import View
 
-def index(request):
-    return HttpResponse("<h1>Felipe Xavier de Carvalho</h1><h1>20221014040007</h1>")
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        latest_question_list = Question.objects.order_by('-pub_date')
+        context = {'latest_question_list': latest_question_list}
+        return render(request, 'enquete/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("Você está olhando para a questão %s." % question_id)
+class DetailView(View):
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk = kwargs['pk'])
+        return render(request, 'enquete/detail.html', {'question': question})
 
-def results(request, question_id):
-    response = "Você está olhando para os resultados da questão %s."
-    return HttpResponse(response % question_id)
+class ResultsView(View):
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk = kwargs['pk'])
+        return render(request, 'enquete/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("Você está votando na questão %s." % question_id)
+class VoteView(View):
+    def post(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk = kwargs['pk'])
+        try:
+            choice_id = request.POST['choice']
+            selected_choice = question.choice_set.get(pk=choice_id)
+        except (KeyError, Choice.DoesNotExist):
+            context = {
+                'question': question,
+                'error_message': "Você precisa selecionar uma alternativa válida!"
+            }
+            # Reexibe o formulário de votação da pergunta.
+            return render(request, 'enquete/detail.html', context)
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Sempre retorna uma HttpResponseRedirect após lidar corretamente cos dados da postagem.
+            # Isso evita que os dados sejam postados duas vezes se um usuário clicar no botão Voltar.
+            return HttpResponseRedirect(reverse('enquete:results', args=(question.id,)))
