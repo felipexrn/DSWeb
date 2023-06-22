ad141a14cb4c0fdb64d71418d40830fab5004ed5
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-06-22 13:15:32 +0000

    Implementação da home

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
         path('admin/', admin.site.urls),
         path('', include('home.urls')),
         path('enquete/', include('enquete.urls')),
+        #path('contabilidade/', include('contabilidate.urls')),
 ]
