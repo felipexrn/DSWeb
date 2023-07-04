22c5376e50db5b6be9af3f4af2e2a327e3975184
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-07-04 00:43:43 +0000

    Models Financas

--- financas/models.py
+++ financas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=200)
+    senha = models.CharField(max_length=8)
+    status = models.BooleanField
+    def __str__(self):
+        return self.nome
+
+class Balancete(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200)
+    data = models.DateTimeField('Data do Balancete')
+    def __str__(self):
+        return self.nome
+
+class Lancamento(models.Model):
+    balancete = models.ForeignKey(Balancete, on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=200)
+    valor = models.FloatField
+    despesa = models.BooleanField
+    def __str__(self):
+        return self.nome
