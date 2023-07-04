f99fac0c4430d315510a64a76998ca31941bb9d3
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2023-07-04 01:58:57 +0000

    migrateeee

--- financas/models.py
+++ financas/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Usuario(models.Model):
     nome = models.CharField(max_length=200)
     senha = models.CharField(max_length=20)
-    status = models.BooleanField
+    status = models.BooleanField(default=False)
     def __str__(self):
         return self.nome
 
@@ -20,7 +20,7 @@ class Lancamento(models.Model):
     balancete = models.ForeignKey(Balancete, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=200)
     valor = models.FloatField(default=0.0)
-    foto = models.ImageField(upload_to='../static/img/financas', default='..static/img/financas/DiagramaDominioFinancas.png')
-    despesa = models.BooleanField
+    foto = models.ImageField(upload_to='static/img/financas', default='static/img/financas/DiagramaDominioFinancas.png')
+    despesa = models.BooleanField(default=False)
     def __str__(self):
         return self.nome
